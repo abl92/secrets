b7b1ef6971e8fee8d137c96eed5322328f66f144
Chronos <ablwebpt@gmail.com>
2023-01-23 18:44:29 -0500

    Adding hashing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const port = 3000;
 const app = express();
@@ -20,8 +20,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"], additionalAuthenticatedFields: ["email"] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -40,7 +38,7 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save((err) => {
         if (!err) {
@@ -57,7 +55,7 @@ app.post("/login", (req, res) => {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === req.body.password) {
+                if (foundUser.password === md5(req.body.password)) {
                     res.render("secrets");
                 }
             }
